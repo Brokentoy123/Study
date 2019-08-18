eb6768e47f88e1bb1de4e8f7d02c64c8137af85a
shiqinghua_notebook <2512171984@qq.com>
2019-08-18 10:18:21 +0800

    素数和reduce应用

---
+++ primeNumber.py
@@ -0,0 +1,23 @@
+def _odd_iter():
+    n = 1
+    while True:
+        n = n + 2
+        yield n
+    
+def _not_disvisible(n):
+    return lambda x:x % n >0
+
+def primes():
+    yield 2
+    it = _odd_iter()#初始化序列
+    while True:
+        n = next(it)
+        yield n
+        it = filter(_not_disvisible(n),it)#构造新序列
+
+if __name__ == "__main__":
+    for n in primes():
+        if n < 1000:
+            print(n)
+        else:
+            break
